22966270399e7759aae4f1703a26daff9800cd6b
Ciro Santilli <ciro.santilli@gmail.com>
2019-03-17 16:12:52 +0000

    callee saved registers

---
+++ compiler-generated/callee_saved_registers.c
@@ -0,0 +1,48 @@
+/* https://stackoverflow.com/questions/18024672/what-registers-are-preserved-through-a-linux-x86-64-function-call/55207335#55207335 */
+
+#include <inttypes.h>
+
+uint64_t inc(uint64_t i) {
+    __asm__ __volatile__(
+        ""
+        : "+m" (i)
+        :
+        : "rax",
+          "rbx",
+          "rcx",
+          "rdx",
+          "rsp",
+          "rsi",
+          "rdi",
+          "r8",
+          "r9",
+          "r10",
+          "r11",
+          "r12",
+          "r13",
+          "r14",
+          "r15",
+          "ymm0",
+          "ymm1",
+          "ymm2",
+          "ymm3",
+          "ymm4",
+          "ymm5",
+          "ymm6",
+          "ymm7",
+          "ymm8",
+          "ymm9",
+          "ymm10",
+          "ymm11",
+          "ymm12",
+          "ymm13",
+          "ymm14",
+          "ymm15"
+    );
+    return i + 1;
+}
+
+int main(int argc, char **argv) {
+    (void)argv;
+    return inc(argc);
+}
